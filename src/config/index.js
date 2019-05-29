9f09ac44216d1bed6ff81aba8e847f03c213d190
Ibrahim Aadlani <ibrahim.aadlani@gmail.com>
2019-05-29 17:44:05 +0200

    edit mongoDB url

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://heroku_r4xb39vl:xukfix-rIjbid-0wuwqe@ds263156.mlab.com:63156/heroku_r4xb39vl",
   "bodyLimit": "100kb"
 }
